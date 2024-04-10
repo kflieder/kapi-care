8441575ff4e40fadf21484e45606c114097f37dd
kflieder <kacey.flieder@gmail.com>
2024-04-09 20:39:20 -0600

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,15 +3,29 @@ import React, { useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import axios, { Axios } from 'axios';
-import { set } from 'mongoose';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Form, FormField, FormControl, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+})
 
 export default function LogInPage() {
   const router = useRouter();
-  const [buttonDisabled, setButtonDisabled] = React.useState(true);
   const [loading, setLoading] = React.useState(false);
   const [user, setUser] = React.useState({
     email: '',
     password: ''
+  })
+
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: user
   })
 
   const onLogin = async () => {
@@ -27,25 +41,47 @@ export default function LogInPage() {
     }
   }
 
-  useEffect(() => {
-    if (user.email.length > 0 && user.password.length > 0) {
-      setButtonDisabled(false)
-    } else {
-      setButtonDisabled(true)
-    }
-  }, [user])
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    setUser(values);
+    onLogin();
+  }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col w-80 border justify-center items-center">
-        <h1>{loading ? "processing" : "Login"}</h1>
-        <label>email</label>
-        <input id="email" type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
-        <label>password</label>
-        <input id="password" type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
-        <button onClick={onLogin} className="border m-5 p-2">{buttonDisabled ? "no sign up" : "sign up"}</button>
-      </div>
+    <div className='flex justify-center items-center h-screen'>
+    <div className='w-80'>
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </div>
     </div>
   )
 }
-
